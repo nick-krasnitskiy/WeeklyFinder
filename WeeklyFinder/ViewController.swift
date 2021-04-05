1178367ba7730c2f3120c4b0ce1a2472e9dce6bb
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-04-05 16:02:43 +0300

    Logic was implemented.

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -16,8 +16,32 @@ class ViewController: UIViewController {
     
     @IBAction func findDayPressed(_ sender: UIButton) {
     
+        // Select current calendar
+        let calendar = Calendar.current
+        
+        // Select necessary date components
+        var dateComponents =  DateComponents()
+        
+        guard let day = dateTextField.text, let month = monthTextField.text, let year = yearTextField.text else {return}
+        dateComponents.day = Int(day) // day
+        dateComponents.month = Int(month) // month
+        dateComponents.year = Int(year) // year
+        
+        // Create date from date componets
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "EEEE" // set the date format
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        let weekday = dateFormatter.string(from: date) // create the final week day
+        
+        resultLabel.text = weekday
     }
     
+    // Hide keyboard by touch
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
 }
 
